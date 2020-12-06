ea5ae7fc92da86208622cd49e2402b10675b49ab
yoshikouki <yoshikouki@pepabo.com>
2020-12-06 18:17:03 +0900

    FizzBuzz 判定部分を切り出した

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,9 @@
+import { toFizzBuzz } from "./lib/FizzBuzz"
+
 const game = () => {
   for (let i = 1; i <= 100; i++) {
     console.log(toFizzBuzz(i))
   }
 }
-
-const toFizzBuzz = (num: number) => {
-  if (num % 3 == 0 && num % 5 == 0) { return 'FizzBuzz' }
-  if (num % 3 == 0) { return 'Fizz' }
-  if (num % 5 == 0) { return 'Buzz' }
-  return num
-}
-
-module.exports = game
 
 game()
---
+++ src/lib/FizzBuzz.ts
@@ -0,0 +1,6 @@
+export function toFizzBuzz(num: number) {
+  if (num % 3 == 0 && num % 5 == 0) { return 'FizzBuzz' }
+  if (num % 3 == 0) { return 'Fizz' }
+  if (num % 5 == 0) { return 'Buzz' }
+  return num
+}
