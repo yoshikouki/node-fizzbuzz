7a0af17da79adba626b02fabc678da70b558aada
yoshikouki <yoshikouki@pepabo.com>
2020-12-06 17:49:58 +0900

    FizzBuzz が動くようになった

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
 const game = () => {
-  console.log(1)
+  for (let i = 1; i <= 100; i++) {
+    console.log(toFizzBuzz(i))
+  }
+}
+
+const toFizzBuzz = (num: number) => {
+  if (num % 3 == 0 && num % 5 == 0) { return 'FizzBuzz' }
+  if (num % 3 == 0) { return 'Fizz' }
+  if (num % 5 == 0) { return 'Buzz' }
+  return num
 }
 
 module.exports = game
